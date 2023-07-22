1fb38aeaed6a24dc0d15c27a30b96b2a7660d720
Utkarsh4517 <uttkarsh.dav@gmail.com>
2023-07-22 13:42:10 +0530

    added heading in graph pages

--- lib/components/combined.dart
+++ lib/components/combined.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
 import 'package:sqltest/constants/colors.dart';
 
 class CombinedGraph extends StatelessWidget {
@@ -6,17 +7,26 @@ class CombinedGraph extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: blackColor,
         iconTheme: const IconThemeData(color: Colors.white),
       ),
       backgroundColor: blackColor,
-      body: Center(
-        child: Text(
-          'Combined',
-          style: TextStyle(color: Colors.white),
-        ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(screenWidth * 0.06),
+            alignment: Alignment.centerLeft,
+            child: GradientText(
+              'Combined Graph ',
+              colors: const [Colors.red, Colors.redAccent, Colors.purple],
+              style:
+                  TextStyle(fontFamily: 'Lexend', fontSize: screenWidth * 0.07, fontWeight: FontWeight.w500),
+            ),
+          ),
+        ],
       ),
     );
   }
