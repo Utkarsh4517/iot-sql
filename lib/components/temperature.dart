3c36fa901a1552114ce012f947644bb13aa71adb
Utkarsh4517 <uttkarsh.dav@gmail.com>
2023-07-22 13:48:45 +0530

    shown temp vs time graph in temp page

--- lib/components/temperature.dart
+++ lib/components/temperature.dart
@@ -1,13 +1,49 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:simple_gradient_text/simple_gradient_text.dart';
 import 'package:sqltest/constants/colors.dart';
+import 'package:sqltest/server.dart';
 
-class TemperatureGraph extends StatelessWidget {
+class TemperatureGraph extends StatefulWidget {
   const TemperatureGraph({super.key});
+
+  @override
+  State<TemperatureGraph> createState() => _TemperatureGraphState();
+}
+
+class _TemperatureGraphState extends State<TemperatureGraph> {
+  // handling database requests
+  var db = MySqlServer();
+  List<Map<String, dynamic>> temperatureData = [];
+  void getTemp() {
+    db.getConnection().then((conn) {
+      String sql = 'select Timestamp, Temperature from IOT';
+      conn.query(sql).then((results) {
+        List<Map<String, dynamic>> tempData = [];
+        for (var row in results) {
+          Map<String, dynamic> rowData = {
+            'Timestamp': row['Timestamp'],
+            'Temperature': row['Temperature'],
+          };
+          tempData.add(rowData);
+        }
+        setState(() {
+          temperatureData = tempData;
+        });
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    getTemp();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: blackColor,
@@ -22,12 +58,44 @@ class TemperatureGraph extends StatelessWidget {
             child: GradientText(
               'Temperature vs Time',
               colors: const [Colors.red, Colors.redAccent, Colors.purple],
-              style:
-                  TextStyle(fontFamily: 'Lexend', fontSize: screenWidth * 0.07, fontWeight: FontWeight.w500),
+              style: TextStyle(
+                  fontFamily: 'Lexend',
+                  fontSize: screenWidth * 0.07,
+                  fontWeight: FontWeight.w500),
             ),
           ),
+          SizedBox(height: screenWidth * 0.1),
+
+          // graph
+          Container(
+            color: Colors.white,
+            margin: const EdgeInsets.all(20),
+            height: 300,
+            child: LineChart(
+              LineChartData(
+                lineBarsData: [
+                  LineChartBarData(
+                    spots: getSpots(),
+                    isCurved: true,
+                    color: Colors.blue,
+                    dotData: const FlDotData(show: false),
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
+  }
+
+  List<FlSpot> getSpots() {
+    List<FlSpot> spots = [];
+    for (int i = 0; i < temperatureData.length; i++) {
+      double x = i.toDouble(); // Use the index as x-value
+      double y = temperatureData[i]['Temperature'].toDouble();
+      spots.add(FlSpot(x, y));
+    }
+    return spots;
   }
 }
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -19,46 +19,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String temp = 'Loading..';
-
-  @override
-  void initState() {
-    getCurrentTemp();
-    getTemp();
-    super.initState();
-  }
-
-  var db = MySqlServer();
-  double temperature = 0;
-  List<Map<String, dynamic>> temperatureData = [];
-
-  void getTemp() {
-    db.getConnection().then((conn) {
-      String sql = 'select Timestamp, Temperature from IOT';
-      conn.query(sql).then((results) {
-        List<Map<String, dynamic>> tempData = [];
-        for (var row in results) {
-          Map<String, dynamic> rowData = {
-            'Timestamp': row['Timestamp'],
-            'Temperature': row['Temperature'],
-          };
-          tempData.add(rowData);
-        }
-        setState(() {
-          temperatureData = tempData;
-        });
-      });
-    });
-  }
-
-  // get current temperature on init state
-
-  void getCurrentTemp(){
-    db.getConnection().then((conn){
-      
-    } );
-  }
-
   String formatTimestamp(int unixTimestamp) {
     var dateTime = DateTime.fromMillisecondsSinceEpoch(unixTimestamp * 1000);
     var formattedDate = DateFormat('yyyy-MM-dd HH:mm:ss').format(dateTime);
@@ -85,47 +45,34 @@ class _HomePageState extends State<HomePage> {
                     fontWeight: FontWeight.bold),
               ),
             ),
-
             SizedBox(height: screenWidth * 0.8),
-
             const Wrap(
               children: [
-                GraphContainer(text: 'Temperature', icon: Icons.thermostat, iconColor: Colors.orange, containerToLoad: TemperatureGraph()),
-                GraphContainer(text: 'Humidity', icon: Icons.thermostat_auto, iconColor: Colors.blue, containerToLoad: HumidityGraph()),
-                GraphContainer(text: 'Coordinates', icon: FeatherIcons.barChart2, iconColor: Colors.white, containerToLoad: CoordinatesGraph()),
-                GraphContainer(text: 'Combined', icon: FeatherIcons.monitor, iconColor: Colors.white, containerToLoad: CombinedGraph()),
+                GraphContainer(
+                    text: 'Temperature',
+                    icon: Icons.thermostat,
+                    iconColor: Colors.orange,
+                    containerToLoad: TemperatureGraph()),
+                GraphContainer(
+                    text: 'Humidity',
+                    icon: Icons.thermostat_auto,
+                    iconColor: Colors.blue,
+                    containerToLoad: HumidityGraph()),
+                GraphContainer(
+                    text: 'Coordinates',
+                    icon: FeatherIcons.barChart2,
+                    iconColor: Colors.white,
+                    containerToLoad: CoordinatesGraph()),
+                GraphContainer(
+                    text: 'Combined',
+                    icon: FeatherIcons.monitor,
+                    iconColor: Colors.white,
+                    containerToLoad: CombinedGraph()),
               ],
             ),
-            
-            // Container(
-            //   margin: const EdgeInsets.all(20),
-            //   height: 300,
-            //   child: LineChart(
-            //     LineChartData(
-            //       lineBarsData: [
-            //         LineChartBarData(
-            //           spots: getSpots(),
-            //           isCurved: true,
-            //           color: Colors.blue,
-            //           dotData: const FlDotData(show: false),
-            //         ),
-            //       ],
-            //     ),
-            //   ),
-            // )
           ],
         ),
       ),
     );
-  }
-
-  List<FlSpot> getSpots() {
-    List<FlSpot> spots = [];
-    for (int i = 0; i < temperatureData.length; i++) {
-      double x = i.toDouble(); // Use the index as x-value
-      double y = temperatureData[i]['Temperature'].toDouble();
-      spots.add(FlSpot(x, y));
-    }
-    return spots;
   }
 }
